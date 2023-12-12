f822c36f8d5155d3427a2784c63db3c693612a96
iak97 <psajithkumar97@gmail.com>
2023-12-12 23:25:56 +0530

    Added user spec validation tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email(domain: 'gmail.com') }
-    password { Faker::Internet.password(min_length: 8) }
+    password { Faker::Internet.password(min_length: 8, special_characters: true) }
     password_confirmation { password } 
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    it "is valid with valid attributes" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it "is atleast 8 characters present" do
+      user = build(:user, password: 'Pass@12', password_confirmation: 'Password@12')
+      expect(user).not_to be_valid
+    end
+
+    it "is atleast one uppercase present" do
+      user = build(:user, password: 'pass@123', password_confirmation: 'pass@123')
+      expect(user).not_to be_valid
+    end
+
+    it "is atleast one special character present" do
+      user = build(:user, password: 'Pass1234', password_confirmation: 'Pass1234')
+      expect(user).not_to be_valid
+    end
+
+    it "is atleast one number present" do
+      user = build(:user, password: 'Password@', password_confirmation: 'Password@')
+      expect(user).not_to be_valid
+    end
+  end
 end
