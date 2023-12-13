f8cc4e65bc559549bcd4ffd4e1645a4914d8aba9
iak97 <psajithkumar97@gmail.com>
2023-12-14 00:40:21 +0530

    Todos request spec index action tests

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :todos
 end
--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Todos", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let!(:todos) { create_list(:todo, 10, user: user) }
+
+  before do
+    sign_in user
+  end
+
+  describe "GET /todos" do
+    
+    before { get '/todos' }
+
+    context 'when todos exist' do
+      it 'returns todos' do
+        expect(response).to have_http_status(:ok)
+
+        expect(json).not_to be_empty
+        expect(json.size).to eq(10)
+      end
+    end
+
+    context 'when todos does not exist' do
+      let(:todos) { }
+
+      it 'returns empty' do
+        expect(response).to have_http_status(:ok)
+
+        expect(json).to be_empty
+      end
+    end
   end
 end
